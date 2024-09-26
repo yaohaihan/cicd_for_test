92d8b782fb13fbe4eb2e9b25db6a029018d79c52
wuliuer <2039900132@qq.com>
2024-09-26 13:45:18 +0800

    testing cicd

--- src/main/java/com/example/demo/Controller/HelloWorldController.java
+++ src/main/java/com/example/demo/Controller/HelloWorldController.java
@@ -1,12 +1,14 @@
 package com.example.demo.Controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-
+@RestController
 @RequestMapping("/hello")
 public class HelloWorldController {
     @GetMapping("/{name}")
