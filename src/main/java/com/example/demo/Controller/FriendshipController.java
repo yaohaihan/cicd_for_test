2bfae053ec4d9b5dedffbfe81a60c4f7a2094159
wuliuer <2039900132@qq.com>
2024-09-26 12:12:47 +0800

    testing cicd

--- src/main/java/com/example/demo/Controller/FriendshipController.java
+++ src/main/java/com/example/demo/Controller/FriendshipController.java
@@ -19,6 +19,7 @@ public class FriendshipController {
     @Autowired
     private UserService userService;
 
+
     @PostMapping("/BuildRelations")
     public Result BuildRelations(@RequestParam int userId1,@RequestParam int userId2){
         if(userService.getUser(userId1)!=null&&userService.getUser(userId2)!=null){
